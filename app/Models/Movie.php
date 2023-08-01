9027dd770c481dea64da8ef82e5e31e9514ac996
kkamara <kamaracomputers@gmail.com>
2023-08-01 12:22:49 +0100

    Get json movie responses

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
+use Illuminate\Support\Facades\Log;
 
 class Movie extends Model
 {
@@ -25,17 +26,22 @@ class Movie extends Model
     }
 
     public function searchApi(string $query, ?int $page=1) {
-        $response = $this->client->request(
-            'GET', 
-            "https://movie-database-alternative.p.rapidapi.com/?s=$query&r=json&page=$page", 
-            [
-                'headers' => [
-                    'X-RapidAPI-Host' => 'movie-database-alternative.p.rapidapi.com',
-                    'X-RapidAPI-Key' => 'f339b4dde8msh3f3d5d13cd057dep151024jsneb855d176177',
-                ],
-            ]
-        );
-        
-        return $response->getBody();
+        try {
+            $response = $this->client->request(
+                'GET', 
+                "https://movie-database-alternative.p.rapidapi.com/?s=$query&r=json&page=$page", 
+                [
+                    'headers' => [
+                        'X-RapidAPI-Host' => 'movie-database-alternative.p.rapidapi.com',
+                        'X-RapidAPI-Key' => 'f339b4dde8msh3f3d5d13cd057dep151024jsneb855d176177',
+                    ],
+                ]
+            );
+            
+            return json_decode((string) $response->getBody());
+        } catch (RequestException $err) {
+            Log::danger($err->getMessage());
+            return false;
+        }
     }
 }
