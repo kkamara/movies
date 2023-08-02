82ffc118fea7f450e57fe30ecf3fee6e8e8aedc8
kkamara <kamaracomputers@gmail.com>
2023-08-02 08:11:01 +0100

    Add feature test

---
+++ tests/Feature/MoviesTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class MoviesTest extends TestCase
+{
+    public function test_a_home_view_can_be_rendered(): void
+    {
+        $view = $this->view('home', ['title' => 'Home']);
+ 
+        $view->assertSee('Home');
+    }
+}
