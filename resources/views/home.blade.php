de515b9d05f3cb17bed970993a18fba1fba1d7ed
kkamara <kamaracomputers@gmail.com>
2023-08-03 09:46:02 +0100

    Fix indentation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,17 +7,17 @@
 
         <div class="container">
             <div class="row">
-            <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
-                <h1>{{ config('app.name') }}</h1>
-                <h2>Search for a movie.</h2>
-                <div class="d-flex">
-                {{-- <a href="javascript:;" class="btn-get-started scrollto">Get Started</a> --}}
-                {{-- <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Watch Video</span></a> --}}
+                <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
+                    <h1>{{ config('app.name') }}</h1>
+                    <h2>Search for a movie.</h2>
+                    <div class="d-flex">
+                    {{-- <a href="javascript:;" class="btn-get-started scrollto">Get Started</a> --}}
+                    {{-- <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Watch Video</span></a> --}}
+                    </div>
                 </div>
-            </div>
-            <div class="col-lg-6 order-1 order-lg-2 hero-img">
-                <img src="{{ asset('images/movie-ge82d2f716_1280.jpg') }}" class="img-fluid animated" alt="">
-            </div>
+                <div class="col-lg-6 order-1 order-lg-2 hero-img">
+                    <img src="{{ asset('images/movie-ge82d2f716_1280.jpg') }}" class="img-fluid animated" alt="">
+                </div>
             </div>
         </div>
 
@@ -28,29 +28,29 @@
         <section id="featured-services" class="featured-services">
             <div class="container">
 
-            <div class="row">
-                <div class="col-lg-4 col-md-6">
-                <div class="icon-box">
-                    <div class="icon"><i class="bi bi-laptop"></i></div>
-                    <h4 class="title"><a href="javascript:;">Lorem Ipsum</a></h4>
-                    <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+                <div class="row">
+                    <div class="col-lg-4 col-md-6">
+                        <div class="icon-box">
+                            <div class="icon"><i class="bi bi-laptop"></i></div>
+                            <h4 class="title"><a href="javascript:;">Lorem Ipsum</a></h4>
+                            <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 mt-4 mt-md-0">
+                        <div class="icon-box">
+                            <div class="icon"><i class="bi bi-card-checklist"></i></div>
+                            <h4 class="title"><a href="javascript:;">Dolor Sitema</a></h4>
+                            <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 mt-4 mt-lg-0">
+                        <div class="icon-box">
+                            <div class="icon"><i class="bi bi-clipboard-data"></i></div>
+                            <h4 class="title"><a href="javascript:;">Sed ut perspiciatis</a></h4>
+                            <p class="sdescription">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+                        </div>
+                    </div>
                 </div>
-                </div>
-                <div class="col-lg-4 col-md-6 mt-4 mt-md-0">
-                <div class="icon-box">
-                    <div class="icon"><i class="bi bi-card-checklist"></i></div>
-                    <h4 class="title"><a href="javascript:;">Dolor Sitema</a></h4>
-                    <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
-                </div>
-                </div>
-                <div class="col-lg-4 col-md-6 mt-4 mt-lg-0">
-                <div class="icon-box">
-                    <div class="icon"><i class="bi bi-clipboard-data"></i></div>
-                    <h4 class="title"><a href="javascript:;">Sed ut perspiciatis</a></h4>
-                    <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
-                </div>
-                </div>
-            </div>
 
             </div>
         </section><!-- End Featured Services Section -->
